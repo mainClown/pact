17d091b7f373e33d4e3d8183b17c0f005efc0126
chermnolesye <119154985+chermnolesye@users.noreply.github.com>
2024-11-14 22:38:44 +0300

    -

--- text_app/views.py
+++ text_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from db_editor.models import Text, Token, PosTag
 
-def show_text(request):
-    # Получаем первый текст из базы данных
-    text = Text.objects.first()
+def show_text(request, text_id=None):
+    # Получаем текст по заданному ID или первый текст, если ID не указан
+    if text_id is not None:
+        text = get_object_or_404(Text, idtext=text_id)  # Если текст с таким ID не найден, вернёт 404
+    else:
+        text = Text.objects.first()  # Используем первый текст по умолчанию
     
     # Получаем токены для данного текста
     tokens = Token.objects.filter(idsentence__idtext=text.idtext)
@@ -11,13 +14,15 @@ def show_text(request):
     # Получаем все части речи
     pos_tags = PosTag.objects.all()
     
-    # Параметры для выбора разметки (по умолчанию 'tagtext', можно выбирать 'tagtextrussian' или 'tagtextabbrev')
+    # Параметры для выбора разметки (по умолчанию 'tagtext', можно выбирать 'tagtextrussian', 'tagtextabbrev' или 'plain')
     selected_markup = request.GET.get('markup', 'tagtext')
     
     # Собираем токены и их данные в список для отображения
     token_data = []
     for token in tokens:
-        if selected_markup == 'tagtextrussian':
+        if selected_markup == 'plain':
+            pos = None  # Не показываем часть речи в режиме 'plain'
+        elif selected_markup == 'tagtextrussian':
             pos = token.idpostag.tagtextrussian if token.idpostag else None
         elif selected_markup == 'tagtextabbrev':
             pos = token.idpostag.tagtextabbrev if token.idpostag else None
