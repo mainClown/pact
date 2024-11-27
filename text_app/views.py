007746eab21097f6d2bb15e2b404e4bad701f2d5
вероятность проеба <150055439+mainClown@users.noreply.github.com>
2024-11-27 22:59:11 +0300

    ended abbr

--- text_app/views.py
+++ text_app/views.py
@@ -4,42 +4,88 @@ from db_editor.models import Text, Token, PosTag
 def show_text(request, text_id=None):
     # Получаем текст по заданному ID или первый текст, если ID не указан
     if text_id is not None:
-        text = get_object_or_404(Text, idtext=text_id)  # Если текст с таким ID не найден, вернёт 404
+        text = get_object_or_404(Text, idtext=text_id)
     else:
-        text = Text.objects.first()  # Используем первый текст по умолчанию
+        text = Text.objects.first()
     
     # Получаем токены для данного текста
     tokens = Token.objects.filter(idsentence__idtext=text.idtext)
     
-    # Получаем все части речи
-    pos_tags = PosTag.objects.all()
-    
-    # Параметры для выбора разметки (по умолчанию 'tagtext', можно выбирать 'tagtextrussian', 'tagtextabbrev' или 'plain')
+    # Параметры для выбора разметки
     selected_markup = request.GET.get('markup', 'tagtext')
     
-    # Собираем токены и их данные в список для отображения
+    # Собираем данные для отображения
     token_data = []
     for token in tokens:
         if selected_markup == 'plain':
             pos = None  # Не показываем часть речи в режиме 'plain'
-        elif selected_markup == 'tagtextrussian':
-            pos = token.idpostag.tagtextrussian if token.idpostag else None
-        elif selected_markup == 'tagtextabbrev':
-            pos = token.idpostag.tagtextabbrev if token.idpostag else None
+            tagcolor = None
         else:
-            pos = token.idpostag.tagtext if token.idpostag else None
+            pos_tag = token.idpostag
+            pos = None
+            tagcolor = None
+            if pos_tag:
+                if selected_markup == 'tagtextrussian':
+                    pos = pos_tag.tagtextrussian
+                elif selected_markup == 'tagtextabbrev':
+                    pos = pos_tag.tagtextabbrev
+                else:
+                    pos = pos_tag.tagtext
+                tagcolor = pos_tag.tagcolor  # Получаем цвет части речи
 
-        token_info = {
+        token_data.append({
             'token': token.tokentext,
             'pos': pos,
-        }
-        token_data.append(token_info)
+            'tagcolor': tagcolor,  # Передаем цвет
+        })
     
     context = {
         'text': text,
         'token_data': token_data,
-        'pos_tags': pos_tags,
         'selected_markup': selected_markup,
     }
 
     return render(request, 'show_text.html', context)
+
+# def show_text(request, text_id=None):
+#     # Получаем текст по заданному ID или первый текст, если ID не указан
+#     if text_id is not None:
+#         text = get_object_or_404(Text, idtext=text_id)  # Если текст с таким ID не найден, вернёт 404
+#     else:
+#         text = Text.objects.first()  # Используем первый текст по умолчанию
+    
+#     # Получаем токены для данного текста
+#     tokens = Token.objects.filter(idsentence__idtext=text.idtext)
+    
+#     # Получаем все части речи
+#     pos_tags = PosTag.objects.all()
+    
+#     # Параметры для выбора разметки (по умолчанию 'tagtext', можно выбирать 'tagtextrussian', 'tagtextabbrev' или 'plain')
+#     selected_markup = request.GET.get('markup', 'tagtext')
+    
+#     # Собираем токены и их данные в список для отображения
+#     token_data = []
+#     for token in tokens:
+#         if selected_markup == 'plain':
+#             pos = None  # Не показываем часть речи в режиме 'plain'
+#         elif selected_markup == 'tagtextrussian':
+#             pos = token.idpostag.tagtextrussian if token.idpostag else None
+#         elif selected_markup == 'tagtextabbrev':
+#             pos = token.idpostag.tagtextabbrev if token.idpostag else None
+#         else:
+#             pos = token.idpostag.tagtext if token.idpostag else None
+
+#         token_info = {
+#             'token': token.tokentext,
+#             'pos': pos,
+#         }
+#         token_data.append(token_info)
+    
+#     context = {
+#         'text': text,
+#         'token_data': token_data,
+#         'pos_tags': pos_tags,
+#         'selected_markup': selected_markup,
+#     }
+
+#     return render(request, 'show_text.html', context)
